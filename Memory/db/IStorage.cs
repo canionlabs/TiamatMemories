8d15cf72a70cf442b1f04f676f1f2825c0856605
caiovictormc <caiovictormc@gmail.com>
2018-12-21 22:55:14 -0300

    Memory.db: Setting IStorage interface

--- Memory/db/IStorage.cs
+++ Memory/db/IStorage.cs
@@ -1,9 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Memory.db
 {
+	public enum StorageType
+	{
+		MONGODB
+
+	}
+
 	public interface IStorage
 	{
-		void Setup();
-		void Save(string @namespace, string data);
+		StorageType StorageType { get; }
+
+		/// <summary>
+		/// Declare your database url
+		/// </summary>
+		/// <param name="url">
+		/// Database url, string following the format mongodb://localhost:27017
+		/// </param>
+		void Setup(string url);
+
+		/// <summary>
+		/// Save an event
+		/// </summary>
+		/// <param name="unixTimestamp">
+		/// A string timestamp follwing the format of the Unix Timestamp
+		/// </param>
+		/// <param name="data">
+		/// Received data
+		/// </param>
+		void Save(string unixTimestamp, string data);
 	}
 }
