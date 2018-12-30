01a9c8aeb75652a5ae29a692d95e7e09730c3ffb
caiovictormc <caiovictormc@gmail.com>
2018-12-30 09:54:36 -0300

    ADDED: Starting Integration Tests to Memory.clients

---
+++ Memory.UnitTests/MQTTClientTests.cs
@@ -0,0 +1,50 @@
+using System;
+using Xunit;
+using MQTTnet.Server;
+using Memory.clients;
+
+namespace Memory.UnitTests
+{
+    public class MQTTFixture:IDisposable
+    {
+        public IMqttServer mqttServer;
+        public MQTTFixture()
+        {
+            StartMQTTServer();
+        }
+
+        public async void StartMQTTServer()
+        {
+            mqttServer = new MQTTnet.MqttFactory().CreateMqttServer();
+            await mqttServer.StartAsync(new MqttServerOptions());
+        }
+
+        public async void StopMQTTServer()
+        {
+            await mqttServer.StopAsync();
+        }
+
+        public void Dispose()
+        {
+            StopMQTTServer();
+            mqttServer = null;
+        }
+    }
+
+    public class MQTTClientTest:IClassFixture<MQTTFixture>
+    {
+        readonly MQTTFixture fixture;
+        public MQTTClientTest(MQTTFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void SetupClient_WithDefaultSettings_ShouldBeAvaliable()
+        {
+            var mqttClient = new MQTTClient();
+            mqttClient.Setup("127.0.0.1", 1883);
+            Assert.True(mqttClient.IsAvailable);
+        }
+    }
+}
